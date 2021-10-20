f2ccccc0b3de162a7d0f9f459799e137174ee25d
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-20 16:50:48 -0300

    ajustes nomenclatura

--- main.tf
+++ main.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # ou ["099720109477"] ID master com permissão para busca
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*"] # exemplo de como listar um nome de AMI - 'aws ec2 describe-images --region us-east-1 --image-ids ami-09e67e426f25ce0d7' https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-images.html
-  }
-}
 
 resource "aws_instance" "k8s_proxy" {
   ami           = "ami-09e67e426f25ce0d7"
@@ -35,7 +22,7 @@ resource "aws_instance" "k8s_proxy" {
   ]
 }
 
-resource "aws_instance" "maquina_master" {
+resource "aws_instance" "k8s_master" {
   ami                         = "ami-09e67e426f25ce0d7"
   instance_type               = "t2.large"
   key_name                    = "key-dev-tf"
@@ -52,7 +39,7 @@ resource "aws_instance" "maquina_master" {
   vpc_security_group_ids = ["${aws_security_group.acessos_master.id}"]
 }
 
-resource "aws_instance" "workers" {
+resource "aws_instance" "k8s_workers" {
   ami                         = "ami-09e67e426f25ce0d7"
   instance_type               = "t2.medium"
   key_name                    = "key-dev-tf"
@@ -234,7 +221,7 @@ resource "aws_security_group" "acessos" {
 # terraform refresh para mostrar o ssh
 output "k8s-masters" {
   value = [
-    for key, item in aws_instance.k8s_masters :
+    for key, item in aws_instance.master :
       "k8s-master ${key+1} - ${item.private_ip}  - ssh -i ~/.ssh/id_rsa_itau ubuntu@${item.public_dns}"
   ]
 }
