019cd413afd3abeb26b95596b0b0358b85c4960a
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-20 16:34:19 -0300

    ajustes para nova infra

--- main.tf
+++ main.tf
@@ -16,9 +16,25 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_instance" "k8s_proxy" {
+  ami                         = "ami-09e67e426f25ce0d7"
+  key_name                    = "key-dev-tf"
+  subnet_id                   = "subnet-0b6ee665d5b518339"
+  instance_type = "t2.micro"
+  root_block_device {
+    encrypted   = true
+    volume_size = 20
+  }
+  key_name      = "treinamento-turma1_itau"
+  tags = {
+    Name = "k8s-haproxy"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acessos.id}"]
+}
+
 resource "aws_instance" "maquina_master" {
   ami                         = "ami-09e67e426f25ce0d7"
-  instance_type               = "t2.medium"
+  instance_type               = "t2.large"
   key_name                    = "key-dev-tf"
   subnet_id                   = "subnet-0b6ee665d5b518339"
   associate_public_ip_address = true
@@ -26,9 +42,13 @@ resource "aws_instance" "maquina_master" {
     encrypted   = true
     volume_size = 20
   }
+  count  = 3
   tags = {
-    Name = "k8s-master-ffaihdw"
+    Name = "k8s-master-ffaihdw-${count.index}"
   }
+  
+}
+
   vpc_security_group_ids = ["${aws_security_group.acessos_master.id}"]
   depends_on = [
     aws_instance.workers,
@@ -37,7 +57,7 @@ resource "aws_instance" "maquina_master" {
 
 resource "aws_instance" "workers" {
   ami                         = "ami-09e67e426f25ce0d7"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   key_name                    = "key-dev-tf"
   subnet_id                   = "subnet-0b6ee665d5b518339"
   associate_public_ip_address = true
@@ -49,7 +69,7 @@ resource "aws_instance" "workers" {
     Name = "k8s-node-ffaihdw-${count.index}"
   }
   vpc_security_group_ids = ["${aws_security_group.acessos_workers.id}"]
-  count                  = 2
+  count                  = 3
 }
 
 
@@ -80,7 +100,7 @@ resource "aws_security_group" "acessos_master" {
       prefix_list_ids  = []
       protocol         = "tcp"
       security_groups = [
-        "sg-0b844b8de4544cf09"
+        "${aws_security_group.acessos_workers.id}",
       ]
       self    = false
       to_port = 65535
@@ -133,7 +153,7 @@ resource "aws_security_group" "acessos_workers" {
       prefix_list_ids  = []
       protocol         = "tcp"
       security_groups = [
-        "sg0575b2e79ee6ebd06",
+        "${aws_security_group.acessos_master.id}",
       ]
       self    = false
       to_port = 65535
@@ -158,6 +178,56 @@ resource "aws_security_group" "acessos_workers" {
     Name = "acessos_workers"
   }
 }
+resource "aws_security_group" "acessos" {
+  name        = "k8s-acessos"
+  description = "acessos inbound traffic"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids  = null,
+      security_groups = null,
+      self = null,
+      description = "Libera dados da rede interna"
+    },
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = [
+        "${aws_security_group.acessos_master.id}",
+      ]
+      self             = false
+      to_port          = 0
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups= null,
+      self= null,
+      description= "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
 
 
 # terraform refresh para mostrar o ssh
@@ -172,5 +242,11 @@ output "aws_instance_e_ssh" {
   value = [
     for key, item in aws_instance.workers :
     "worker ${key + 1} - ${item.public_ip} - ssh -i ~/.ssh/id_rsa ubuntu@${item.public_dns}"
+  ]
+}
+  
+  output "output-k8s_proxy" {
+  value = [
+    "k8s_proxy - ${aws_instance.k8s_proxy.private_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.k8s_proxy.public_dns}"
   ]
 }
